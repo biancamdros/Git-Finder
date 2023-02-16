2b62a0c83d923b6d7512fc4998bb12c7c8751b2b
biancamdros <bibilaise@gmail.com>
2023-02-16 17:29:00 -0300

    Melhorias

--- github_api/src/routes/Home.tsx
+++ github_api/src/routes/Home.tsx
@@ -2,13 +2,22 @@ import Search from "./components/Search";
 import { useState } from "react";
 import { UserProps } from "../types/user";
 import User from "./components/User";
+import Error from "./components/Error";
+
 
 const Home = () => {
     const [user, setUser] = useState <UserProps|null> (null);
+    const [error, setError] = useState(false);
     const loadUser = async(username: string) => {
+        setError(false);
+        setUser(null); 
         const res = await fetch(`https://api.github.com/users/${username}`)
 
         const data = await res.json();
+        if (res.status === 404) {
+            setError(true);
+            return;
+        }
         const {avatar_url, login, location, followers, following} = data
 
         const userData: UserProps = {
@@ -24,6 +33,7 @@ const Home = () => {
         <div> 
             <Search loadUser = {loadUser}/> 
             {user && <User {...user}/>}
+            {error && <Error/>}
         </div>
     )  
 
